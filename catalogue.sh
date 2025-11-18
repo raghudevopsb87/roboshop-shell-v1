d3d9207a9c698c6d6cd3d432a3a22c26d4656217
Raghu K <raghuk.vit@gmail.com>
2025-11-18 07:54:06 +0530

    Make catalogue script

---
+++ catalogue.sh
@@ -0,0 +1,26 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+
+dnf install nodejs -y
+
+useradd roboshop
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+
+cd /app
+npm install
+
+dnf install mongodb-mongosh -y
+mongosh --host mongodb-dev.rdevopsb87.online </app/db/master-data.js
+
+
+systemctl enable catalogue
+systemctl restart catalogue
+
