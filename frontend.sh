2d5b11aadb5f0d0f11701918c6024f53b95512d5
Raghu K <raghuk.vit@gmail.com>
2025-11-18 07:28:30 +0530

    Roboshop frontend script - Change the order of nginx config file copy

--- frontend.sh
+++ frontend.sh
@@ -5,12 +5,11 @@ dnf install nginx -y
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-
-
 rm -f /etc/nginx/nginx.conf
 cp nginx.conf /etc/nginx/nginx.conf
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
 
 
 systemctl enable nginx
